fea7e47511c0f3fd4c116c17baff65863649d9c8
JDonaireF <joe.brad.2030@gmail.com>
2023-09-27 23:16:35 -0400

    Funcionalidad de listado de medidores

---
+++ app/components/Meter.jsx
@@ -0,0 +1,54 @@
+'use client';
+import Link from 'next/link';
+
+function CardMeters({dataMeters}) {
+  return (
+    <div className="grid grid-cols-4 gap-8">
+        {dataMeters.map(meter => (
+        <div key={meter.Id} className="max-w-sm rounded overflow-hidden shadow-lg bg-gray-200">
+            <div className="px-6 py-4">
+                <div className="font-bold text-center text-blue-600 mb-2">Medidor {meter.NumeroMedidor}</div>
+                <p className="text-gray-700 text-sm font-medium">
+                    Cliente: Esteban Quito Lozada
+                </p>
+            </div>
+            <div className="rounded bg-white mx-4 p-2 text-sm">
+                <div className="grid grid-cols-2 gap-8 mt-6">
+                    <div className="text-center">
+                        <p className="font-bold p-2">Consumo</p>
+                        <p>{meter.Consumo} W</p>
+                    </div>
+                    <div className="text-center">
+                        <p className="font-bold p-2">Hora/Registro</p>
+                        <p>{new Date(meter.FechaActualizacion).toLocaleTimeString()}</p>
+                    </div>
+                    <div className="text-center">
+                        <p className="font-bold p-2">Lectura</p>
+                        <p>{meter.Lectura}</p>
+                    </div>
+                    <div className="text-center">
+                        <p className="font-bold p-2">Total</p>
+                        <p>40 Bs.</p>
+                    </div>
+                </div>
+                <div className="text-center p-4 mt-4">
+                    <Link href="/history" className="bg-black hover:bg-slate-400 text-white text-sm font-bold py-2 px-4 rounded">
+                        VER DEUDAS
+                    </Link>
+                </div>
+            </div>
+            <div class="m-2 text-center">
+                <button className="bg-transparent hover:bg-black text-black hover:text-white border border-black text-sm font-bold m-2 p-2 rounded">
+                    DESHABILITAR
+                </button>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white text-sm font-bold m-2 p-2 rounded">
+                    HABILITAR
+                </button>
+            </div>
+        </div>
+        ))}
+    </div>
+  );
+}
+
+export default CardMeters;
--- app/history/page.jsx
+++ app/history/page.jsx
@@ -1,123 +1,90 @@
+import Link from "next/link";
 import React from "react";
 
 function History() {
     return(
         <div className="m-6 rounded bg-white p-8">
-            <div class="grid grid-cols-6 gap-2 p-4 text-sm">
-                <div class="bg-gray-300 rounded p-2">Enero</div>
-                <div class="bg-gray-300 rounded p-2">Febrero</div>
-                <div class="bg-gray-300 rounded p-2">Marzo</div>
-                <div class="bg-gray-300 rounded p-2">Abril</div>
-                <div class="bg-gray-300 rounded p-2">Mayo</div>
-                <div class="bg-gray-300 rounded p-2">Junio</div>
-                <div class="bg-gray-300 rounded p-2">Julio</div>
-                <div class="bg-gray-300 rounded p-2">Agosto</div>
-                <div class="bg-gray-300 rounded p-2">Septiembre</div>
-                <div class="bg-gray-300 rounded p-2">Obtubre</div>
-                <div class="bg-gray-300 rounded p-2">Noviembre</div>
-                <div class="bg-gray-300 rounded p-2">Diciembre</div>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-white uppercase bg-blue-600 dark:text-white">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                Consumo
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Lectura
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Total
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Fecha
+                            </th>
+                            <th className="py-3">
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="bg-white border-b dark:bg-white dark:border-gray-400">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
+                                5.2W
+                            </th>
+                            <td className="px-6 py-4">
+                                7831
+                            </td>
+                            <td className="px-6 py-4">
+                                40 Bs.
+                            </td>
+                            <td className="px-6 py-4">
+                                20/08/2023
+                            </td>
+                            <td className="px-6 py-4">
+                            <Link href="/" className="px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Generar factura
+                            </Link>
+                            </td>
+                        </tr>
+                        <tr class="bg-white border-b dark:bg-white dark:border-gray-400">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
+                                5.2W
+                            </th>
+                            <td className="px-6 py-4">
+                                7831
+                            </td>
+                            <td className="px-6 py-4">
+                                40 Bs.
+                            </td>
+                            <td className="px-6 py-4">
+                                20/08/2023
+                            </td>
+                            <td className="px-6 py-4">
+                            <Link href="/" className="px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Generar factura
+                            </Link>
+                            </td>
+                        </tr>
+                        <tr className="bg-white dark:bg-white">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
+                                5.2W
+                            </th>
+                            <td className="px-6 py-4">
+                                7831
+                            </td>
+                            <td className="px-6 py-4">
+                                40 Bs.
+                            </td>
+                            <td className="px-6 py-4">
+                                20/08/2023
+                            </td>
+                            <td className="px-6 py-4">
+                            <Link href="/" className="px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Generar factura
+                            </Link>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-            <table class="table-fixed w-full text-sm border-collapse text-center bg-slate-100 border border-gray-300 rounded">
-                <thead>
-                    <tr>
-                    <th class="border-b p-2">Consumo</th>
-                    <th class="border-b p-2">Lectura</th>
-                    <th class="border-b p-2">Total</th>
-                    <th class="border-b p-2">Fecha</th>
-                    <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <td class="border-b p-2">5.2W</td>
-                    <td class="border-b p-2">7831</td>
-                    <td class="border-b p-2">40 Bs.</td>
-                    <td class="border-b p-2">20/08/2023</td>
-                    <td class="border-b p-2">
-                        <button class="bg-blue-500 hover:bg-blue-300 text-white text-sm font-bold py-2 px-4 rounded">
-                            Detalles
-                        </button>
-                    </td>
-                    </tr>
-                    <tr>
-                    <td class="border-b p-2">5.2W</td>
-                    <td class="border-b p-2">7831</td>
-                    <td class="border-b p-2">40 Bs.</td>
-                    <td class="border-b p-2">20/08/2023</td>
-                    <td class="border-b p-2">
-                        <button class="bg-blue-500 hover:bg-blue-300 text-white text-sm font-bold py-2 px-4 rounded">
-                            Detalles
-                        </button>
-                    </td>
-                    </tr>
-                    <tr>
-                    <td class="border-b p-2">5.2W</td>
-                    <td class="border-b p-2">7831</td>
-                    <td class="border-b p-2">40 Bs.</td>
-                    <td class="border-b p-2">20/08/2023</td>
-                    <td class="border-b p-2">
-                        <button class="bg-blue-500 hover:bg-blue-300 text-white text-sm font-bold py-2 px-4 rounded">
-                            Detalles
-                        </button>
-                    </td>
-                    </tr>
-                    <tr>
-                    <td class="border-b p-2">5.2W</td>
-                    <td class="border-b p-2">7831</td>
-                    <td class="border-b p-2">40 Bs.</td>
-                    <td class="border-b p-2">20/08/2023</td>
-                    <td class="border-b p-2">
-                        <button class="bg-blue-500 hover:bg-blue-300 text-white text-sm font-bold py-2 px-4 rounded">
-                            Detalles
-                        </button>
-                    </td>
-                    </tr>
-                    <tr>
-                    <td class="border-b p-2">5.2W</td>
-                    <td class="border-b p-2">7831</td>
-                    <td class="border-b p-2">40 Bs.</td>
-                    <td class="border-b p-2">20/08/2023</td>
-                    <td class="border-b p-2">
-                        <button class="bg-blue-500 hover:bg-blue-300 text-white text-sm font-bold py-2 px-4 rounded">
-                            Detalles
-                        </button>
-                    </td>
-                    </tr>
-                    <tr>
-                    <td class="border-b p-2">5.2W</td>
-                    <td class="border-b p-2">7831</td>
-                    <td class="border-b p-2">40 Bs.</td>
-                    <td class="border-b p-2">20/08/2023</td>
-                    <td class="border-b p-2">
-                        <button class="bg-blue-500 hover:bg-blue-300 text-white text-sm font-bold py-2 px-4 rounded">
-                            Detalles
-                        </button>
-                    </td>
-                    </tr>
-                    <tr>
-                    <td class="border-b p-2">5.2W</td>
-                    <td class="border-b p-2">7831</td>
-                    <td class="border-b p-2">40 Bs.</td>
-                    <td class="border-b p-2">20/08/2023</td>
-                    <td class="border-b p-2">
-                        <button class="bg-blue-500 hover:bg-blue-300 text-white text-sm font-bold py-2 px-4 rounded">
-                            Detalles
-                        </button>
-                    </td>
-                    </tr>
-                    <tr>
-                    <td class="border-b p-2">5.2W</td>
-                    <td class="border-b p-2">7831</td>
-                    <td class="border-b p-2">40 Bs.</td>
-                    <td class="border-b p-2">20/08/2023</td>
-                    <td class="border-b p-2">
-                        <button class="bg-blue-500 hover:bg-blue-300 text-white text-sm font-bold py-2 px-4 rounded">
-                            Detalles
-                        </button>
-                    </td>
-                    </tr>
-                </tbody>
-            </table>
         </div>
     )
 }
